9ad18a47eb3599f34e5046001816647e026d4d08
Igor Budasov <igor.budasov@gmail.com>
2015-03-03 19:07:53 +0200

    Default config

---
+++ config/default.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "ENV": 'dev',
+    "AUTH_CALLBACK_URL": "http://localhost:3000/",
+    "MONGO_DB": "mongodb://localhost/minder"
+};
--- controllers/default.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "ENV": 'dev',
-    "AUTH_CALLBACK_URL": "http://localhost:3000/",
-    "MONGO_DB": "mongodb://localhost/tagorama",
-    "IG_CLIENT_ID": "47403fe2177142c0a6b1b550c140818f",
-    "IG_CLIENT_SECRET": "c46876a16b98441898f1a9d1569be85c",
-    "RELATIONS_UPDATE_TIME": 115 * 60 * 1000,
-    "SNAPSHOT_IG_REQUEST_INTERVAL": 30
-};
