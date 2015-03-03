55de8ffd0af3d025e17d93cca0ded3e061937342
Igor Budasov <igor.budasov@gmail.com>
2015-03-03 19:05:22 +0200

    Removing policy controlling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var thoughtController = require('../controllers/ThoughtController.js');
-//var policy = require('../lib/policy.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {res.render('index', { title: 'Express' });});
