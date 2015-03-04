1cb724c190fa69a415553d6456f01575e5a29e31
Igor Budasov <igor.budasov@gmail.com>
2015-03-04 18:05:30 +0200

    Heroku config

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "ENV": 'dev',
+    "AUTH_CALLBACK_URL": "https://minder-dev.herokuapp.com/",
+    "MONGO_DB": "mongodb://me:ok@ds029787.mongolab.com:29787/heroku_app34570337"
+};
