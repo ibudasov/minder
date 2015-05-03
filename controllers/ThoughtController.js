732ed5d7145a3f101a2ba3b3888fd31eeba25482
Igor Budasov <igor.budasov@gmail.com>
2015-05-03 15:18:04 +0300

    Reverse order for the list of thoughts

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -16,33 +16,50 @@ function add(req, res) {
 
 function get(req, res) {
     var thoughtsResult = [];
-    Thought.find({}, function (err, foundThoughts) {
-        if (err) return errs.handle(err);
+    Thought.find()
+        .sort({'createdAt': -1})
+        .exec(function (err, foundThoughts) {
+            if (err) return errs.handle(err);
 
-        foundThoughts.forEach(function(thought) {
-            thoughtsResult.push(thought.itself);
+            foundThoughts.forEach(function (thought) {
+                thoughtsResult.push(thought.itself);
+            });
+
+            res.json({
+                list: thoughtsResult
+            });
         });
-        res.json({
-            list: thoughtsResult
-        });
-    });
 }
 
 function getDistinct(req, res) {
     var thoughtsResult = [];
-    Thought.distinct('itself', {}, function (err, foundThoughts) {
-        if (err) return errs.handle(err);
 
-        foundThoughts.forEach(function(thought) {
-            thoughtsResult.push(thought);
+    /**
+     * There is no possibility to use distinct with sort, so I've solved problem
+     * with aggregation function;
+     * @see: http://stackoverflow.com/questions/27968930/sort-and-distinct-in-mongoose
+     */
+    Thought.aggregate(
+        {
+            $group: {
+                _id: "$itself"
+            }
+        },
+        {$sort: {createdAt: -1}},
+        function (err, foundThoughts) {
+            if (err) return errs.handle(err);
+            foundThoughts.forEach(function (thought) {
+                thoughtsResult.push(thought._id);
+            });
+
+            res.json({
+                list: thoughtsResult
+            });
         });
-        res.json({
-            list: thoughtsResult
-        });
-    });
 }
 
-function _saveNewThought(thought) {
+function _saveNewThought(thought, callback) {
+    // todo: make this code async, refactor
     // todo: enrich Thought object with geo-location, client info, etc
     // todo: check of countToday, and increase if needed
     var newThought = new Thought({
