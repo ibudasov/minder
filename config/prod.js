59dac05784c8bc32d1933dba38929ac6836efa37
Igor Budasov <igor.budasov@gmail.com>
2015-05-21 17:01:40 +0300

    Login with Google -- prod configuration

--- config/prod.js
+++ config/prod.js
@@ -6,8 +6,7 @@ module.exports = {
     "FB_CLIENT_ID": "1060391333988383",
     "FB_CALLBACK_URL": "https://minder-dev.herokuapp.com/auth/facebook/callback",
 
-    "GOOGLE_CLIENT_SECRET": "",
-    "GOOGLE_CLIENT_ID": "",
+    "GOOGLE_CLIENT_SECRET": "S1kkrcGVqQAcoDl9ca52U912",
+    "GOOGLE_CLIENT_ID": "740818259438-2mu0h7cdcs6ddqdt01c1j7jgg5uaudlb.apps.googleusercontent.com",
     "GOOGLE_CALLBACK_URL": "https://minder-dev.herokuapp.com/auth/google/callback"
-
 };
