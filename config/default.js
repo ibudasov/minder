d727d02b0a81d5ce21f77778d97bfdedce643613
Igor Budasov <igor.budasov@gmail.com>
2015-05-27 17:55:10 +0300

    New config for dev application

--- config/default.js
+++ config/default.js
@@ -2,8 +2,8 @@ module.exports = {
     "ENV": 'dev',
     "MONGO_DB": "mongodb://localhost/minder",
 
-    "FB_CLIENT_SECRET": "f3de9964deae139a42a3bdd6ddb01374",
-    "FB_CLIENT_ID": "1582560042032661",
+    "FB_CLIENT_SECRET": "85481029caf17f07d5e445b1ff123d87",
+    "FB_CLIENT_ID": "1077973092230207",
     "FB_CALLBACK_URL": "http://localhost:3000/auth/facebook/callback",
 
     "GOOGLE_CLIENT_SECRET": "tTPY2Cek1c_uR7AhxKGc3kzX",
