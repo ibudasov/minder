132b3e78b22b2661c63f9ab6e289e9b2fb0411f8
Maksym <m.husiev@gmail.com>
2015-05-27 12:43:55 +0300

    Try to use sourceTree

---
+++ Копия iOS/Minder/Minder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Minder
+//
+//  Created by Igor Budasov on 23.05.15.
+//  Copyright (c) 2015 Igor Budasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
